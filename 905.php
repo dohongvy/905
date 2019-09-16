6b7e41380408ae5df4837b1408408ea661492e31
dohongvy <vykhtn1998@gmail.com>
2019-09-16 23:15:53 +0700

    update 905
    update 905

--- 905.php
+++ 905.php
@@ -1,3 +1,17 @@
+<?php
+$url_host = 'http://' . $_SERVER['HTTP_HOST'];
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+preg_match_all($pattern_uri, __DIR__, $matches);
+$url_path = $url_host . $matches[1][0];
+$url_path = str_replace('\\', '/', $url_path);
+if (!class_exists('lessc')) {
+    include ('./libs/lessc.inc.php');
+}
+$less = new lessc;
+$less->compileFile('less/905.less', 'css/905.css');
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,97 +20,17 @@
     <meta name="robots" content="noindex, nofollow">
     <title>Login Screen with Form - Bootsnipp.com</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <link href="css/905.css" rel="stylesheet" type="text/css" />
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-    <script src="js/s.js"></script>
+    <link href="<?php echo $url_path ?>/css/bootstrap-4.1.1.min.css" rel="stylesheet">
+    <link href="<?php echo $url_path ?>/css/905.css" rel="stylesheet" type="text/css" />
+    <script src="<?php echo $url_path ?>/js/jquery-3.2.1.min.js"></script>
+    <script  src="<?php echo $url_path ?>/js/bootstrap-4.1.1.min.js"></script>
+    <script src="js/905.js"></script>
 </head>
 
 <body>
-    <section class="login-block">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4 login-sec">
-                    <h2 class="text-center">Login Now</h2>
-                    <!-- begin form login -->
-                    <div class="login-form">
-                        <div class="form-group">
-                            <label class="text-uppercase">Username</label>
-                            <input type="text" id="username" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <label class="text-uppercase">Password</label>
-                            <input type="password" id="password" class="form-control">
-                        </div>
-                        <div class="form-check">
-                            <label class="form-check-label">
-                                <input type="checkbox" class="form-check-input" id="remeberMe">
-                                <label class="form-check-label" for="remeberMe">Remember Me</label>
-                            </label>
-                            <button id="btnLogin" class="btn btn-login float-right">Login</button>
-                        </div>
-                    </div>
-                    <!-- end form login -->
-                    <div class="copyright-text">Created with <i class="fa fa-heart"></i> by <a
-                            href="http://grafreez.com">Grafreez.com</a></div>
-                </div>
-                <div class="col-md-8 banner-sec">
-                    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                        <ol class="carousel-indicators">
-                            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                        </ol>
-                        <div class="carousel-inner" role="listbox">
-                            <div class="carousel-item active">
-                                <img class="d-block img-fluid"
-                                    src="https://znews-photo.zadn.vn/w1024/Uploaded/jaroin/2017_03_19/crouching_lion_michael_matti.jpg"
-                                    alt="First slide">
-                                <div class="carousel-caption d-none d-md-block">
-                                    <div class="banner-text">
-                                        <h2>This is Heaven</h2>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                            tempor incididunt ut
-                                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                                            exercitation</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="carousel-item">
-                                <img class="d-block img-fluid"
-                                    src="https://blogtraitim.info/wp-content/uploads/2018/06/Hinh-anh-dep-3.jpg"
-                                    alt="First slide">
-                                <div class="carousel-caption d-none d-md-block">
-                                    <div class="banner-text">
-                                        <h2>This is Heaven</h2>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                            tempor incididunt ut
-                                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                                            exercitation</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="carousel-item">
-                                <img class="d-block img-fluid"
-                                    src="http://thuvienanhdep.net/wp-content/uploads/2015/10/hinh-anh-thien-nhien-3d-tuyet-dep-va-an-tuong-cho-hinh-nen-may-tinh-8.jpg"
-                                    alt="First slide">
-                                <div class="carousel-caption d-none d-md-block">
-                                    <div class="banner-text">
-                                        <h2>This is Heaven</h2>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                            tempor incididunt ut
-                                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                                            exercitation</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+<?php
+    include './905-content.php';
+?>
 </body>
 
 </html>
